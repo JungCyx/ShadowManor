e45876f5c2274873d43b7c0c8f4fa4810ad0d532
JungCyx <shekhhsalman@gmail.com>
2025-05-06 22:10:25 -0700

    added finish game to the end

--- finish.gd
+++ finish.gd
@@ -1,16 +1,24 @@
 extends Area3D
 
+@export var game_end_label: Label  # Reference to the GameEnd label
 
-# Called when the node enters the scene tree for the first time.
+var detection_enabled = false
+
 func _ready() -> void:
-	pass # Replace with function body.
+	if game_end_label:
+		game_end_label.visible = false
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+	# Enable detection after 0.5 seconds
+	await get_tree().create_timer(0.5).timeout
+	detection_enabled = true
 
 func _on_body_entered(body: Node3D) -> void:
-	if body.name == "Player":
+	if detection_enabled and body.name == "Player":
 		print("Game ended")
+		
+		var label = get_node_or_null("/root/Node3D/CanvasLayer/GameEnd")
+		if label:
+			label.text = "You Escape the ShadowManor!"
+			label.visible = true
+		else:
+			print("GameEnd label not found!")
